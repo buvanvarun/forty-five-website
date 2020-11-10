309218f50e426660d19488c557ca6457eb0c537a
ThejasBK <thejaskiran99@gmail.com>
2020-11-11 01:05:54 +0530

    Changes done to navbar. Working on the video rendering

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  
+  initState = false;
+  hamburger;
+  navLinks;
+  links;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.hamburger = document.querySelector('.hamburger');
+    this.navLinks = document.querySelector('.nav-links');
+    this.links = document.querySelectorAll('.nav-links li');
+    console.log(this.navLinks, this.hamburger, this.links);
   }
+
+  onBtnToggle = () => {
+    this.navLinks.classList.toggle('open');
+  };
 
 }
